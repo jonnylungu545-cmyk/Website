59b504521872b24f2d3043cb8d5d59441a2f603f
jonnylungu545-cmyk <jonnylungu545@gmail.com>
2026-04-23 22:25:12 +0300

    Delete cript.js

--- cript.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('action-btn').addEventListener('click', function() {
-    alert('Bravo! Codul tău funcționează și interacționează cu utilizatorul.');
-    console.log('Butonul a fost apăsat!');
-});
