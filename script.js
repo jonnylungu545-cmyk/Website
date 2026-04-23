2a3c509409cfd22eb975f2b4b35b44914d13c6c4
jonnylungu545-cmyk <jonnylungu545@gmail.com>
2026-04-23 21:39:12 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('action-btn').addEventListener('click', function() {
+    alert('Bravo! Codul tău funcționează și interacționează cu utilizatorul.');
+    console.log('Butonul a fost apăsat!');
+});
