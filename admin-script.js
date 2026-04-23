0ed2a6e67e4e3c4c887f91211c997ecddfa632d1
jonnylungu545-cmyk <jonnylungu545@gmail.com>
2026-04-23 22:37:47 +0300

    Update admin-script.js

--- admin-script.js
+++ admin-script.js
@@ -1,4 +1,4 @@
-const SB_URL = "https://exsrkldqdatytexqgeey.supabase.co/rest/v1/";
+const SB_URL = "https://exsrkldqdatytexqgeey.supabase.co";
 const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4c3JrbGRxZGF0eXRleHFnZWV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NTc4ODUsImV4cCI6MjA5MjUzMzg4NX0.i3YOfcuojy3YEuc0o1rH7QIxJUM4aaYhaE4g3Dy2eak"; // Lipsește cheia ta aici între ghilimele
 
 document.getElementById('btn-save').addEventListener('click', async () => {
